bb35bf0a4773daf55d77cdc46c5bf04725b7e4f3
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2022-06-01 17:18:37 +0100

    setup the command argument to call the help text

--- meditate.go
+++ meditate.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/M-Derbyshire/meditate-cli/help"
+)
+
 func main() {
+
+	args := os.Args[1:]
+
+	if len(args) > 0 {
+
+		if args[0] == "help" {
+			helpText := help.GetHelpText()
+			fmt.Println(helpText)
+		}
+
+	}
 
 }
